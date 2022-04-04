7eeedf5e52c37d0443f9af160cccce0bfca6385a
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2022-04-04 21:20:40 +0200

    Update convexe.m

--- Convexe/convexe.m
+++ Convexe/convexe.m
@@ -5,7 +5,7 @@
 % sont entrés ligne par ligne (i.e. la première colonne représente les coordonnées
 % sur l'axe X et la seconde représente les coordonnées sur l'axes Y)
 
-% Version : 1.0
+% Version : 1.1
 % Author : Cellier R.
 
 function chemin_convexe = convexe(M)
@@ -13,20 +13,19 @@ function chemin_convexe = convexe(M)
   % séparation des coordonnées des points contenus dans la matrice M
   X = M(:,1);
   Y = M(:,2);
-
   % affiche l'ensemble des points de coordonnées (x,y)
   figure; subplot(1,2,1)
   plot(X,Y, '+'); axis("square");
 
   % defini les coordonnées du point le plus à gauche (abscisse la plus petite)
-  P_Gauche = [X(min(X)==X), Y(min(X)==X)];
+  P_Gauche = [X(min(X)==X), Y(min(X)==X)](1,:);
   % defini les coordonnées du point le plus à droite (abscisse la plus grande)
-  P_Droite = [X(max(X)==X), Y(max(X)==X)];
+  P_Droite = [X(max(X)==X), Y(max(X)==X)](1,:);
 
   % voir chemin_convexe_haut
-  convexeH = chemin_convexe_haut(P_Gauche, P_Droite, X,Y);
+  convexeH = chemin_convexe_haut(P_Gauche, P_Droite, M);
   % voir chemin_convexe_bas
-  convexeB = chemin_convexe_bas(P_Gauche, P_Droite, X,Y);
+  convexeB = chemin_convexe_bas(P_Gauche, P_Droite, M);
   % assemble les chemins haut et bas pour former le chemin final de l'enveloppe
   % convexe (par convention, le point de départ est le point le plus à gauche)
   chemin_convexe = [convexeH(1:end-1,:);convexeB(end:-1:1,:)];
