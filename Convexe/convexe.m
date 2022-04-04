415500b405de488481290e8ad037e19686080da7
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2022-04-04 20:01:53 +0200

    Update convexe.m

--- Convexe/convexe.m
+++ Convexe/convexe.m
@@ -10,28 +10,28 @@
 
 function chemin_convexe = convexe(M)
   assert(ismatrix(M), "M doit être une matrice")
-  # séparation des coordonnées des points contenus dans la matrice M
+  % séparation des coordonnées des points contenus dans la matrice M
   X = M(:,1);
   Y = M(:,2);
 
-  # affiche l'ensemble des points de coordonnées (x,y)
+  % affiche l'ensemble des points de coordonnées (x,y)
   figure; subplot(1,2,1)
   plot(X,Y, '+'); axis("square");
 
-  # defini les coordonnées du point le plus à gauche (abscisse la plus petite)
+  % defini les coordonnées du point le plus à gauche (abscisse la plus petite)
   P_Gauche = [X(min(X)==X), Y(min(X)==X)];
-  # defini les coordonnées du point le plus à droite (abscisse la plus grande)
+  % defini les coordonnées du point le plus à droite (abscisse la plus grande)
   P_Droite = [X(max(X)==X), Y(max(X)==X)];
 
-  # voir chemin_convexe_haut
+  % voir chemin_convexe_haut
   convexeH = chemin_convexe_haut(P_Gauche, P_Droite, X,Y);
-  # voir chemin_convexe_bas
+  % voir chemin_convexe_bas
   convexeB = chemin_convexe_bas(P_Gauche, P_Droite, X,Y);
-  # assemble les chemins haut et bas pour former le chemin final de l'enveloppe
-  # convexe (par convention, le point de départ est le point le plus à gauche)
+  % assemble les chemins haut et bas pour former le chemin final de l'enveloppe
+  % convexe (par convention, le point de départ est le point le plus à gauche)
   chemin_convexe = [convexeH(1:end-1,:);convexeB(end:-1:1,:)];
 
-  # affiche l'ensemble des points et trace l'enveloppe convexe
+  % affiche l'ensemble des points et trace l'enveloppe convexe
   subplot(1,2,2)
   plot(X,Y, '+');
   axis("square"); hold on;
