20f56c4f5fd76f868b761ef31cfc48f72481c920
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2022-04-04 19:56:55 +0200

    Update vect.m

--- Convexe/vect.m
+++ Convexe/vect.m
@@ -1,5 +1,11 @@
+% usage : coord = direction_vect(p1,p2)
+%
 % Renvoie les coordonnées de direction du vecteur p1->p2
-function [a,b] = vect(p1,p2)
+
+% Version : 1.0
+% Author : Cellier R.
+
+function [a,b] = direction_vect(p1,p2)
   a = p2(1)-p1(1);
   b = p2(2)-p1(2);
 endfunction
